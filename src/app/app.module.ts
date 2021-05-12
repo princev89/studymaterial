7840a5b1ad66ec085dff091a87c03a396d98e5f2
princev89 <vishwakarmaprince833@gmail.com>
2021-05-12 20:51:08 +0530

    added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExploreComponent } from './explore/explore.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {path: 'explore', component: ExploreComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ExploreComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
